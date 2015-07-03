5a2da4e563bf6f580a9822b867c51da3a0afa48e
Jacob Buckheit <jbman223@gmail.com>
2015-07-03 17:12:16 -0400

    Create site styles and index page.

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jacob
- * Date: 7/3/15
- * Time: 4:55 PM
- */ 
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/site.css" rel="stylesheet">
+</head>
+
+<body>
+<div class="container body-content">
+        <h1>Early AP Scores</h1>
+
+        <p class="lead">Get access to your AP scores on June 6th no matter where you are located. Enter your College Board username and password below and you will be redirected to your score report page. Don't worry though - your login information is never stored and we have no reason to want it anyways. Want more proof? View this project on GitHub: <a href="https://github.com/jbman223/EarlyAP">https://github.com/jbman223/EarlyAP</a>, you could even host your own if you still don't feel comfortable using the public version.</p>
+</div>
+<!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
